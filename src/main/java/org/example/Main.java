ac4e4391b792dec36a6c22fb0132872819666464
MarlonVB <104545159+MarlonVB@users.noreply.github.com>
2022-08-27 20:20:50 -0500

    correccion de errores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        boolean isNumero = true;
-        boolean numeroMayor = true;
-        boolean divCero = true;
+
         Validacion validacion = new Validacion();
         Scanner scn = new Scanner(System.in);
         String n1, n2;
@@ -20,29 +18,32 @@ public class Main {
 
             if (!validacion.ingresoObligatorio(n1)){
                 System.out.println("DEBE INGRESAR UN VALOR");
+            }else {
+                // * Valido que los datos sean numericos
+                if (!validacion.isNumeric(n1)){
+                    System.out.println("LOS DATOS INGRESADOS DEBEN SER NUMERICOS");
+                }else {
+                    // * Valido que los datos no se mayores a 100000
+                    if (!validacion.validarMayorque(n1)){
+                        System.out.println("NO PUEDE INGRESAR VALORES MAYORES A 100000");
+                    }
+                }
             }
-            // * Valido que los datos sean numericos
-            if (!validacion.isNumeric(n1)){
-                System.out.println("LOS DATOS INGRESADOS DEBEN SER NUMERICOS");
-            }
-            // * Valido que los datos no se mayores a 100000
-            if (!validacion.validarMayorque(n1)){
-                System.out.println("NO PUEDE INGRESAR VALORES MAYORES A 100000");
-            }
-        }while (!validacion.ingresoObligatorio(n1)|| !validacion.isNumeric(n1)|| !validacion.validarMayorque(n1));
+        }while (!validacion.ingresoObligatorio(n1) || !validacion.isNumeric(n1)|| !validacion.validarMayorque(n1));
 
         do{
             System.out.println("Ingrese el segundo numero");
             n2 = scn.nextLine();
             if (!validacion.ingresoObligatorio(n2)){
                 System.out.println("DEBE INGRESAR UN VALOR");
-            }
-            if (!validacion.isNumeric(n2)){
-                System.out.println("LOS DATOS INGRESADOS DEBEN SER NUMERICOS");
-            }
-            // * Valido que los datos no se mayores a 100000
-            if (!validacion.validarMayorque(n2)){
-                System.out.println("NO PUEDE INGRESAR VALORES MAYORES A 100000");
+            }else {
+                if (!validacion.isNumeric(n2)){
+                    System.out.println("LOS DATOS INGRESADOS DEBEN SER NUMERICOS");
+                }else {
+                    if (!validacion.validarMayorque(n2)){
+                        System.out.println("NO PUEDE INGRESAR VALORES MAYORES A 100000");
+                    }
+                }
             }
         }while (!validacion.ingresoObligatorio(n2) || !validacion.isNumeric(n2) || !validacion.validarMayorque(n2));
 
